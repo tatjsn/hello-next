4702ca2b2e6b9be8f4efa631d70a36cf1358394d
Taz Jackson <tatjsn@users.noreply.github.com>
2023-05-16 22:02:42 +0100

    Fix build error

--- app/hoge/page.js
+++ app/hoge/page.js
@@ -1,8 +1,15 @@
 import { cookies } from 'next/headers';
 
-export default function Hoge() {
+async function getPosition() {
+  'use server';
   const latitude = cookies().get('latitude').value;
   const longitude = cookies().get('longitude').value;
+
+  return [latitude, longitude];
+}
+
+export default async function Hoge() {
+  const [latitude, longitude] = await getPosition();
 
   return (
     <div>
